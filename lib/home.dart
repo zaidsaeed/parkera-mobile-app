9b8c994454a1f4e90b2be28b471e044dd9a79fec
Zaid <zaidsaeed>
2020-01-29 14:10:55 -0500

    added the add car button

--- lib/home.dart
+++ lib/home.dart
@@ -4,7 +4,18 @@ import 'package:flutter/material.dart';
 class Home extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    // TODO: implement build
-    return Text('Home');
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Floating Action Button'),
+      ),
+      body: Center(child: const Text('Press the button below!')),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          // Add your onPressed code here!
+        },
+        child: Icon(Icons.add),
+        backgroundColor: Colors.green,
+      ),
+    );
   }
 }

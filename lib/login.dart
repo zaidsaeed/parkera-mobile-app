aa40fee07a51f4ad7682e04d09c143f73c21385c
WeihaoH <33268129+WeihaoH@users.noreply.github.com>
2020-02-29 16:53:50 -0500

    add global id

--- lib/login.dart
+++ lib/login.dart
@@ -7,6 +7,8 @@ import './login/loginSupport.dart';
 import 'package:graphql/client.dart';
 import 'package:password/password.dart';
 import 'package:parkera/home.dart';
+import 'package:provider/provider.dart';
+import './state/AppState.dart';
 
 
 class Login extends StatefulWidget {
@@ -22,177 +24,188 @@ class _LoginState extends State<Login> {
 
   @override
   Widget build(BuildContext context) {
-    return new Scaffold(
-        resizeToAvoidBottomPadding: false,
-        body: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: <Widget>[
-              Container(
-                child: Stack(
+    return Provider<AppState>(
+        create:(context)=> AppState(),
+        child: MaterialApp(
+          home: new Scaffold(
+              resizeToAvoidBottomPadding: false,
+              body: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
                   children: <Widget>[
                     Container(
-                      padding: EdgeInsets.fromLTRB(15.0, 110.0, 0.0, 0.0),
-                      child: Text(
-                        'Log In',
-                        style: TextStyle(
-                            fontSize: 80.0,
-                            fontWeight: FontWeight.bold,
-                            fontFamily: 'Lato'),
+                      child: Stack(
+                        children: <Widget>[
+                          Container(
+                            padding: EdgeInsets.fromLTRB(15.0, 110.0, 0.0, 0.0),
+                            child: Text(
+                              'Log In',
+                              style: TextStyle(
+                                  fontSize: 80.0,
+                                  fontWeight: FontWeight.bold,
+                                  fontFamily: 'Lato'),
+                            ),
+                          )
+                        ],
                       ),
-                    )
-                  ],
-                ),
-              ),
-              Container(
-                  padding: EdgeInsets.only(top: 35.0, left: 20.0, right: 20.0),
-                  child: Column(
-                    children: <Widget>[
-                      TextField(
-                        onChanged: (text) {
-                          setState(() {
-                            _AccountInfo["email"] = text;
-                          });
-                          print(_AccountInfo);
-                        },
-                        decoration: InputDecoration(
-                            labelText: 'EMAIL',
-                            labelStyle: TextStyle(
-                                fontFamily: 'Lato',
-                                fontWeight: FontWeight.bold,
-                                color: Colors.grey),
-                            // hintText: 'EMAIL',
-                            // hintStyle: ,
-                            focusedBorder: UnderlineInputBorder(
-                                borderSide: BorderSide(color: Colors.green))),
-                      ),
-                      SizedBox(height: 10.0),
-                      TextField(
-                        onChanged: (text) {
-                          setState(() {
-                            _AccountInfo["password"] = text;
-                          });
-                          print(_AccountInfo);
-                        },
-                        decoration: InputDecoration(
-                            labelText: 'PASSWORD ',
-                            labelStyle: TextStyle(
-                                fontFamily: 'Lato',
-                                fontWeight: FontWeight.bold,
-                                color: Colors.grey),
-                            focusedBorder: UnderlineInputBorder(
-                                borderSide: BorderSide(color: Colors.green))),
-                        obscureText: true,
-                      ),
-                      SizedBox(height: 10.0),
-
-                      // TextField(
-                      //   decoration: InputDecoration(
-                      //       labelText: 'NICK NAME ',
-                      //       labelStyle: TextStyle(
-                      //           fontFamily: 'Montserrat',
-                      //           fontWeight: FontWeight.bold,
-                      //           color: Colors.grey),
-                      //       focusedBorder: UnderlineInputBorder(
-                      //           borderSide: BorderSide(color: Colors.green))),
-                      // ),
-                      // SizedBox(height: 50.0),
-                      Container(
-                          height: 40.0,
-                          child: Material(
-                            borderRadius: BorderRadius.circular(20.0),
-                            shadowColor: Colors.tealAccent,
-                            color: Colors.teal,
-                            elevation: 7.0,
-                            child: FlatButton(
-                              onPressed: () async {
-                                final GraphQLClient _client =
-                                    graphQLConfiguration.clientToQuery();
-                                var _userdata = _AccountInfo.values.toList();
-                                final QueryResult result =
-                                    await _client.query(QueryOptions(
-                                  documentNode:
-                                      gql(loginSupport.readRepositories),
-                                  variables: <String, dynamic>{
-                                    'nEmail': _userdata[0],
-                                  },
-                                ));
-
-                                if (result.hasException) {
-                                  print(result.exception.toString());
-                                }
-                                String reL = result.data['getAuthenticationbyEmail'][0]['password'];
-
-                               if(Password.verify(_userdata[1], reL)){
-                                 int uid = result.data['getAuthenticationbyEmail'][0]['userAccountId'];
-                                 Navigator.push(
-                                   context,
-                                   MaterialPageRoute(
-                                       builder: (context) => Home()),
-                                 );
-                               }
-
-
-
-                                // var _userdata = _AccountInfo.values.toList();
-                                // print(_userdata[0]);
-                                // Query(options: QueryOptions(
-                                //     documentNode: gql(loginSupport.readRepositories),
-                                //      variables: {
-                                //     'nEmail': _userdata[0],
-                                //   }
-
-                                // ),
-                                //     builder: (QueryResult result,{ VoidCallback refetch, FetchMore fetchMore }) {
-                                //       if (result.hasException) {
-
-                                //         return Text(result.exception.toString());
-                                //       }
-                                //       if (result==null) {
-                                //         return Text("NO data found");
-                                //       }
-                                //   print("adasd"+result.data);
-                                //   return Text("yes we have");
-                                // });
+                    ),
+                    Container(
+                        padding: EdgeInsets.only(top: 35.0, left: 20.0, right: 20.0),
+                        child: Column(
+                          children: <Widget>[
+                            TextField(
+                              onChanged: (text) {
+                                setState(() {
+                                  _AccountInfo["email"] = text;
+                                });
+                                print(_AccountInfo);
                               },
-                              child: Center(
-                                child: Text(
-                                  'Login',
-                                  style: TextStyle(
-                                      color: Colors.white,
+                              decoration: InputDecoration(
+                                  labelText: 'EMAIL',
+                                  labelStyle: TextStyle(
+                                      fontFamily: 'Lato',
                                       fontWeight: FontWeight.bold,
-                                      fontFamily: 'Lato'),
+                                      color: Colors.grey),
+                                  // hintText: 'EMAIL',
+                                  // hintStyle: ,
+                                  focusedBorder: UnderlineInputBorder(
+                                      borderSide: BorderSide(color: Colors.green))),
+                            ),
+                            SizedBox(height: 10.0),
+                            TextField(
+                              onChanged: (text) {
+                                setState(() {
+                                  _AccountInfo["password"] = text;
+                                });
+                                print(_AccountInfo);
+                              },
+                              decoration: InputDecoration(
+                                  labelText: 'PASSWORD ',
+                                  labelStyle: TextStyle(
+                                      fontFamily: 'Lato',
+                                      fontWeight: FontWeight.bold,
+                                      color: Colors.grey),
+                                  focusedBorder: UnderlineInputBorder(
+                                      borderSide: BorderSide(color: Colors.green))),
+                              obscureText: true,
+                            ),
+                            SizedBox(height: 10.0),
+
+                            // TextField(
+                            //   decoration: InputDecoration(
+                            //       labelText: 'NICK NAME ',
+                            //       labelStyle: TextStyle(
+                            //           fontFamily: 'Montserrat',
+                            //           fontWeight: FontWeight.bold,
+                            //           color: Colors.grey),
+                            //       focusedBorder: UnderlineInputBorder(
+                            //           borderSide: BorderSide(color: Colors.green))),
+                            // ),
+                            // SizedBox(height: 50.0),
+                            Container(
+                                height: 40.0,
+                                child: Material(
+                                  borderRadius: BorderRadius.circular(20.0),
+                                  shadowColor: Colors.tealAccent,
+                                  color: Colors.teal,
+                                  elevation: 7.0,
+                                  child: Consumer<AppState>(
+                                    builder: (context,appstate,child){
+                                      return FlatButton(
+                                        onPressed: () async {
+                                          final GraphQLClient _client =
+                                          graphQLConfiguration.clientToQuery();
+                                          var _userdata = _AccountInfo.values.toList();
+                                          final QueryResult result =
+                                          await _client.query(QueryOptions(
+                                            documentNode:
+                                            gql(loginSupport.readRepositories),
+                                            variables: <String, dynamic>{
+                                              'nEmail': _userdata[0],
+                                            },
+                                          ));
+
+                                          if (result.hasException) {
+                                            print(result.exception.toString());
+                                          }
+                                          String reL = result.data['getAuthenticationbyEmail'][0]['password'];
+
+                                          if(Password.verify(_userdata[1], reL)){
+
+                                            appstate.setuserid(result.data['getAuthenticationbyEmail'][0]['userAccountId']);
+                                            Navigator.push(
+                                              context,
+                                              MaterialPageRoute(
+                                                  builder: (context) => Home()),
+                                            );
+                                          }
+
+
+
+                                          // var _userdata = _AccountInfo.values.toList();
+                                          // print(_userdata[0]);
+                                          // Query(options: QueryOptions(
+                                          //     documentNode: gql(loginSupport.readRepositories),
+                                          //      variables: {
+                                          //     'nEmail': _userdata[0],
+                                          //   }
+
+                                          // ),
+                                          //     builder: (QueryResult result,{ VoidCallback refetch, FetchMore fetchMore }) {
+                                          //       if (result.hasException) {
+
+                                          //         return Text(result.exception.toString());
+                                          //       }
+                                          //       if (result==null) {
+                                          //         return Text("NO data found");
+                                          //       }
+                                          //   print("adasd"+result.data);
+                                          //   return Text("yes we have");
+                                          // });
+                                        },
+                                        child: Center(
+                                          child: Text(
+                                            'Login',
+                                            style: TextStyle(
+                                                color: Colors.white,
+                                                fontWeight: FontWeight.bold,
+                                                fontFamily: 'Lato'),
+                                          ),
+                                        ),
+                                      );
+                                    }
+                                  )
+                                )
+                            ),
+                            SizedBox(height: 20.0),
+                            Container(
+                              height: 40.0,
+                              color: Colors.transparent,
+                              child: Container(
+                                decoration: BoxDecoration(
+                                    border: Border.all(
+                                        color: Colors.black,
+                                        style: BorderStyle.solid,
+                                        width: 1.0),
+                                    color: Colors.transparent,
+                                    borderRadius: BorderRadius.circular(20.0)),
+                                child: InkWell(
+                                  onTap: () {
+                                    Navigator.of(context).pop();
+                                  },
+                                  child: Center(
+                                    child: Text('Go Back',
+                                        style: TextStyle(
+                                            fontWeight: FontWeight.bold,
+                                            fontFamily: 'Lato')),
+                                  ),
                                 ),
                               ),
                             ),
-                          )),
-                      SizedBox(height: 20.0),
-                      Container(
-                        height: 40.0,
-                        color: Colors.transparent,
-                        child: Container(
-                          decoration: BoxDecoration(
-                              border: Border.all(
-                                  color: Colors.black,
-                                  style: BorderStyle.solid,
-                                  width: 1.0),
-                              color: Colors.transparent,
-                              borderRadius: BorderRadius.circular(20.0)),
-                          child: InkWell(
-                            onTap: () {
-                              Navigator.of(context).pop();
-                            },
-                            child: Center(
-                              child: Text('Go Back',
-                                  style: TextStyle(
-                                      fontWeight: FontWeight.bold,
-                                      fontFamily: 'Lato')),
-                            ),
-                          ),
-                        ),
-                      ),
-                    ],
-                  )),
-              // GqlCaller()
-            ]));
+                          ],
+                        )),
+                    // GqlCaller()
+                  ]))
+        )
+    );
   }
 }
---
+++ lib/state/AppState.dart
@@ -0,0 +1,8 @@
+class AppState{
+  int userid = 0;
+
+  void setuserid(int id){
+    userid = id;
+  }
+
+}

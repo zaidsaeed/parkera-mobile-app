8fdbebf676376ddb2c74ac5ab62970a42551f45d
zaidsaeed <zaidsaeed>
2020-03-06 17:36:07 -0500

    need to test logic before final commit

--- lib/signup/signup.dart
+++ lib/signup/signup.dart
@@ -113,123 +113,138 @@ class _SignUpState extends State<SignUp> {
     }
 
     return new Scaffold(
-        resizeToAvoidBottomPadding: false,
-        body: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: <Widget>[
-              SignUpHeader(),
-              Container(
-                  padding: EdgeInsets.only(top: 35.0, left: 20.0, right: 20.0),
-                  child: Column(
-                    children: <Widget>[
-                      SignUpTextField(
-                        labelText: 'First Name',
-                        onTextChange: (text) {
-                          setState(() {
-                            _userInfo["firstname"] = text;
-                          });
-                        },
-                        emptyErrorText: 'First Name Cannot Be Empty',
-                        textEditingController: firstNameTextFieldController,
-                        isValid: true,
-                        isEmpty: isEmptyMap['isFirstNameEmpty'],
-                      ),
-                      SizedBox(height: 10.0),
-                      SignUpTextField(
-                        labelText: 'Last Name',
-                        onTextChange: (text) {
-                          setState(() {
-                            _userInfo["lastname"] = text;
-                          });
-                        },
-                        emptyErrorText: 'Last Name Cannot Be Empty',
-                        textEditingController: lastNameTextFieldController,
-                        isValid: true,
-                        isEmpty: isEmptyMap['isLastNameEmpty'],
-                      ),
-                      SizedBox(height: 10.0),
-                      SignUpTextField(
-                        labelText: 'Email',
-                        onTextChange: (text) {
-                          setState(() {
-                            _userInfo["email"] = text;
-                          });
-                        },
-                        emptyErrorText: 'Email Cannot Be Empty',
-                        invalidErrorText: 'Email is invalid',
-                        textEditingController: emailTextFieldController,
-                        isValid: isEmailValid,
-                        isEmpty: isEmptyMap['isEmailEmpty'],
-                      ),
-                      SizedBox(height: 10.0),
-                      SignUpTextField(
-                          labelText: 'Phone',
+      resizeToAvoidBottomPadding: false,
+      body: Builder(
+          builder: (context) =>
+              Column(crossAxisAlignment: CrossAxisAlignment.start, children: <
+                  Widget>[
+                SignUpHeader(),
+                Container(
+                    padding:
+                        EdgeInsets.only(top: 35.0, left: 20.0, right: 20.0),
+                    child: Column(
+                      children: <Widget>[
+                        SignUpTextField(
+                          labelText: 'First Name',
                           onTextChange: (text) {
                             setState(() {
-                              _userInfo["phone"] = text;
+                              _userInfo["firstname"] = text;
                             });
                           },
-                          emptyErrorText: 'Phone Cannot Be Empty',
-                          textEditingController: phoneTextFieldController,
-                          isEmpty: isEmptyMap['isPhoneEmpty'],
-                          isValid: true),
-                      SizedBox(height: 10.0),
-                      SignUpTextField(
-                        labelText: 'Password',
-                        onTextChange: (text) {
-                          setState(() {
-                            _userInfo["password"] = text;
-                          });
-                        },
-                        emptyErrorText: 'Password Cannot Be Empty',
-                        textEditingController: passwordTextFieldController,
-                        isEmpty: isEmptyMap['isPasswordEmpty'],
-                        isValid: true,
-                      ),
-                      SizedBox(height: 10.0),
-                      Mutation(
-                        options: MutationOptions(
-                          documentNode: gql(addUserMutation
-                              .addUser), // this is the mutation string you just created
+                          emptyErrorText: 'First Name Cannot Be Empty',
+                          textEditingController: firstNameTextFieldController,
+                          isValid: true,
+                          isEmpty: isEmptyMap['isFirstNameEmpty'],
                         ),
-                        builder: (
-                          RunMutation runMutation,
-                          QueryResult result,
-                        ) {
-                          return Container(
-                            height: 40.0,
-                            color: Colors.transparent,
-                            child: Container(
-                                decoration: BoxDecoration(
-                                    border: Border.all(
-                                        color: Colors.black,
-                                        style: BorderStyle.solid,
-                                        width: 1.0),
-                                    color: Colors.transparent,
-                                    borderRadius: BorderRadius.circular(20.0)),
-                                child: Center(
-                                  child: FlatButton(
-                                    onPressed: () {
-                                      print(_userInfo);
-                                      bool isValid = validateSignUp();
-                                      if (isValid) {
-                                        var _userdata =
-                                            _userInfo.values.toList();
-                                        runMutation({
-                                          'firstname': _userdata[0],
-                                          'lastname': _userdata[1],
-                                          'email': _userdata[2],
-                                          'phone': _userdata[3],
-                                          'user_role': 'User',
-                                          'password': Password.hash(
-                                              _userdata[4], new PBKDF2())
-                                        });
-                                        if (result.hasException) {
-                                          Scaffold.of(context)
-                                              .showSnackBar(SnackBar(
-                                            content: Text(
-                                                'Something went wrong. This email might already be associated to an account'),
-                                          ));
+                        SizedBox(height: 10.0),
+                        SignUpTextField(
+                          labelText: 'Last Name',
+                          onTextChange: (text) {
+                            setState(() {
+                              _userInfo["lastname"] = text;
+                            });
+                          },
+                          emptyErrorText: 'Last Name Cannot Be Empty',
+                          textEditingController: lastNameTextFieldController,
+                          isValid: true,
+                          isEmpty: isEmptyMap['isLastNameEmpty'],
+                        ),
+                        SizedBox(height: 10.0),
+                        SignUpTextField(
+                          labelText: 'Email',
+                          onTextChange: (text) {
+                            setState(() {
+                              _userInfo["email"] = text;
+                            });
+                          },
+                          emptyErrorText: 'Email Cannot Be Empty',
+                          invalidErrorText: 'Email is invalid',
+                          textEditingController: emailTextFieldController,
+                          isValid: isEmailValid,
+                          isEmpty: isEmptyMap['isEmailEmpty'],
+                        ),
+                        SizedBox(height: 10.0),
+                        SignUpTextField(
+                            labelText: 'Phone',
+                            onTextChange: (text) {
+                              setState(() {
+                                _userInfo["phone"] = text;
+                              });
+                            },
+                            emptyErrorText: 'Phone Cannot Be Empty',
+                            textEditingController: phoneTextFieldController,
+                            isEmpty: isEmptyMap['isPhoneEmpty'],
+                            isValid: true),
+                        SizedBox(height: 10.0),
+                        SignUpTextField(
+                          labelText: 'Password',
+                          onTextChange: (text) {
+                            setState(() {
+                              _userInfo["password"] = text;
+                            });
+                          },
+                          emptyErrorText: 'Password Cannot Be Empty',
+                          textEditingController: passwordTextFieldController,
+                          isEmpty: isEmptyMap['isPasswordEmpty'],
+                          isValid: true,
+                        ),
+                        SizedBox(height: 10.0),
+                        Mutation(
+                          options: MutationOptions(
+                            documentNode: gql(addUserMutation
+                                .addUser), // this is the mutation string you just created
+                          ),
+                          builder: (
+                            RunMutation runMutation,
+                            QueryResult result,
+                          ) {
+                            return Container(
+                              height: 40.0,
+                              color: Colors.transparent,
+                              child: Container(
+                                  decoration: BoxDecoration(
+                                      border: Border.all(
+                                          color: Colors.black,
+                                          style: BorderStyle.solid,
+                                          width: 1.0),
+                                      color: Colors.transparent,
+                                      borderRadius:
+                                          BorderRadius.circular(20.0)),
+                                  child: Center(
+                                    child: FlatButton(
+                                      onPressed: () {
+                                        print(_userInfo);
+                                        bool isValid = validateSignUp();
+                                        if (isValid) {
+                                          runMutation({
+                                            'firstname': _userInfo['firstname'],
+                                            'lastname': _userInfo['lastname'],
+                                            'email': _userInfo['email'],
+                                            'phone': _userInfo['phone'],
+                                            'user_role': 'User',
+                                            'password': Password.hash(
+                                                _userInfo['password'],
+                                                new PBKDF2())
+                                          });
+                                          if (result.hasException) {
+                                            Scaffold.of(context)
+                                                .showSnackBar(SnackBar(
+                                              content: Text(result.exception
+                                                  .graphqlErrors[0].message),
+                                            ));
+                                          } else {
+                                            Scaffold.of(context)
+                                                .showSnackBar(SnackBar(
+                                              content: Text(
+                                                  'User has been created.'),
+                                            ));
+
+                                            Navigator.push(
+                                              context,
+                                              MaterialPageRoute(
+                                                  builder: (context) => Home()),
+                                            );
+                                          }
                                         } else {
                                           Scaffold.of(context)
                                               .showSnackBar(SnackBar(
@@ -237,64 +252,58 @@ class _SignUpState extends State<SignUp> {
                                                 'Please refer to the errors included in the form.'),
                                           ));
                                         }
-                                      } else {
-                                        Scaffold.of(context)
-                                            .showSnackBar(SnackBar(
-                                          content: Text(
-                                              'Please refer to the errors included in the form.'),
-                                        ));
-                                      }
-                                    },
-                                    child: Text(
-                                      "Sign Up",
-                                      style: TextStyle(fontFamily: 'Lato'),
+                                      },
+                                      child: Text(
+                                        "Sign Up",
+                                        style: TextStyle(fontFamily: 'Lato'),
+                                      ),
                                     ),
-                                  ),
-                                )),
-                          );
-                        },
-                      ),
-                      SizedBox(height: 20.0),
-                      Container(
-                        height: 40.0,
-                        color: Colors.transparent,
-                        child: Container(
-                          decoration: BoxDecoration(
-                              border: Border.all(
-                                  color: Colors.black,
-                                  style: BorderStyle.solid,
-                                  width: 1.0),
-                              color: Colors.transparent,
-                              borderRadius: BorderRadius.circular(20.0)),
-                          child: InkWell(
-                            onTap: () {
-                              Navigator.of(context).pop();
-                            },
-                            child: Center(
-                              child: Text('Go Back',
-                                  style: TextStyle(
-                                      fontWeight: FontWeight.bold,
-                                      fontFamily: 'Lato')),
+                                  )),
+                            );
+                          },
+                        ),
+                        SizedBox(height: 20.0),
+                        Container(
+                          height: 40.0,
+                          color: Colors.transparent,
+                          child: Container(
+                            decoration: BoxDecoration(
+                                border: Border.all(
+                                    color: Colors.black,
+                                    style: BorderStyle.solid,
+                                    width: 1.0),
+                                color: Colors.transparent,
+                                borderRadius: BorderRadius.circular(20.0)),
+                            child: InkWell(
+                              onTap: () {
+                                Navigator.of(context).pop();
+                              },
+                              child: Center(
+                                child: Text('Go Back',
+                                    style: TextStyle(
+                                        fontWeight: FontWeight.bold,
+                                        fontFamily: 'Lato')),
+                              ),
                             ),
                           ),
                         ),
-                      ),
-                    ],
-                  )),
-              Center(
-                child: FlatButton(
-                  onPressed: () {
-                    Navigator.push(
-                      context,
-                      MaterialPageRoute(builder: (context) => Home()),
-                    );
-                  },
-                  child: Text(
-                    "Navigate To Homepage",
-                    style: TextStyle(fontFamily: 'Lato'),
+                      ],
+                    )),
+                Center(
+                  child: FlatButton(
+                    onPressed: () {
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(builder: (context) => Home()),
+                      );
+                    },
+                    child: Text(
+                      "Navigate To Homepage",
+                      style: TextStyle(fontFamily: 'Lato'),
+                    ),
                   ),
-                ),
-              )
-            ]));
+                )
+              ])),
+    );
   }
 }

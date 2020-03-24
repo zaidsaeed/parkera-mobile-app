336b362b87a96dfaceb3df401a4859d61620631d
zaidsaeed <zaidsaeed>
2020-03-23 21:28:59 -0400

    fixed hamburger menu

--- lib/home/home.dart
+++ lib/home/home.dart
@@ -8,6 +8,7 @@ import 'package:toast/toast.dart';
 class Home extends StatelessWidget {
   final String snackbarText;
   Home({this.snackbarText});
+  final GlobalKey<ScaffoldState> _scaffoldKey = new GlobalKey<ScaffoldState>();
 
   @override
   Widget build(BuildContext context) {
@@ -20,13 +21,16 @@ class Home extends StatelessWidget {
 
     return Scaffold(
       extendBodyBehindAppBar: true,
+      key: _scaffoldKey,
       appBar: AppBar(
           backgroundColor: Colors.transparent,
           elevation: 0,
-          leading: new Icon(
-            Icons.menu,
-            color: Colors.black,
-          )),
+          leading: new IconButton(
+              icon: Icon(Icons.menu),
+              color: Colors.black,
+              onPressed: () {
+                _scaffoldKey.currentState.openDrawer();
+              })),
       drawer: HomeDrawer(),
       floatingActionButton: HomeSpeedDial(),
       body: Center(
